a2b9277f15a1ed945795a3957fd5dc5e1e4cc10b
Yadheedhya <yadheedhya6@gmail.com>
2023-11-14 19:51:32 +0530

    beta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
         <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
           <p className='text-sm font-semibold text-gray-700'>
-            DocXpert <span style={{ borderBottom: '2px solid red' }}>Under Development!</span>
+            DocXpert is in <span style={{ borderBottom: '2px solid red' }}>Beta!</span>
           </p>
 
         </div>
